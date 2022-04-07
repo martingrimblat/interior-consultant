9a56fb183296127c60630a03a16ef690bb48b674
martín <martin.grimblat@gmail.com>
2022-04-06 21:21:57 -0300

    agrego .js . funciona el botón pero se desacomoda el estilo de la lista

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector (".hamburger")
+const links = document.querySelector (".links")
+
+hamburger.addEventListener("click", () => {
+	hamburger.classList.toggle("active")
+	links.classList.toggle("links")
+}
+)
